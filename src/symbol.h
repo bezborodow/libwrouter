2af9377ca3bcfe71ceb98124ab2aa8b023b55b5d
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-10 05:11:51 +0930

    Symbol intern prototype.

--- src/symbol.h
+++ src/symbol.h
@@ -1,18 +1,24 @@
 #include <stdint.h>
+#include <stddef.h>
 
 #ifndef WROUTER_SYMBOL_H
 #define WROUTER_SYMBOL_H
+
+typedef uint16_t symbol_t;
 
 typedef const char * const symbol_table_entry_t;
 
 typedef struct {
     symbol_table_entry_t *table;
     uint16_t count;
+    uint16_t capacity;
 } symbol_table_t;
 
 typedef struct {
     symbol_table_t literals;
     symbol_table_t params;
 } symbol_ctx_t;
+
+symbol_t symbol_intern(symbol_table_t *tbl, const char *str, size_t len);
 
 #endif
