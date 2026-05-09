74ff704e44cc79fbc6acfcd9362446e82d8095b1
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-09 21:43:44 +0930

    Lexer test symbols.

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -9,26 +9,49 @@
 
 _Static_assert(sizeof(token_t) == 4, "token_t must be 4 bytes");
 
-enum {
-    SYMBOL_ACTION = 0,
-    SYMBOL_SEGMENT,
-    SYMBOL_USERS,
-    SYMBOL_POSTS,
-    SYMBOL_API,
-    SYMBOL_V1,
-    SYMBOL_ACCOUNT,
-    SYMBOL_ACCOUNT_ID,
-    SYMBOL_PROJECT,
-    SYMBOL_EDIT,
-    SYMBOL_STATIC,
-    SYMBOL_A,
-    SYMBOL_B,
-    SYMBOL_C,
-    SYMBOL_D,
+typedef enum {
+    LITERAL_A = 0,
+    LITERAL_ACCOUNT,
+    LITERAL_API,
+    LITERAL_B,
+    LITERAL_C,
+    LITERAL_D,
+    LITERAL_EDIT,
+    LITERAL_POSTS,
+    LITERAL_PROJECT,
+    LITERAL_STATIC,
+    LITERAL_USERS,
+    LITERAL_V1,
+    LITERAL_COUNT
+} literal_id_t;
 
-    SYMBOL_ID = 0,
-    SYMBOL_NAME,
-    SYMBOL_PATH
+static const char *literal_table[] = {
+    "a",
+    "account",
+    "account_id",
+    "api",
+    "b",
+    "c",
+    "d",
+    "edit",
+    "posts",
+    "project",
+    "static",
+    "users",
+    "v1"
+};
+
+typedef enum {
+    PARAM_ACCOUNT_ID = 0,
+    PARAM_ID,
+    PARAM_NAME,
+    PARAM_COUNT
+} param_id_t;
+
+static const char *param_table[] = {
+    "account_id",
+    "id",
+    "name"
 };
 
 typedef struct {
@@ -47,7 +70,7 @@ static lexer_test_t tests[] = {
     {
         .input = "/users",
         .expected = {
-            { TOKEN_LITERAL, 5, SYMBOL_USERS },
+            { TOKEN_LITERAL, 5, LITERAL_USERS },
         },
         .expected_count = 1,
     },
@@ -55,7 +78,7 @@ static lexer_test_t tests[] = {
     {
         .input = "/users/",
         .expected = {
-            { TOKEN_LITERAL, 5, SYMBOL_USERS },
+            { TOKEN_LITERAL, 5, LITERAL_USERS },
         },
         .expected_count = 1,
     },
@@ -63,8 +86,8 @@ static lexer_test_t tests[] = {
     {
         .input = "/users/posts",
         .expected = {
-            { TOKEN_LITERAL, 5, SYMBOL_USERS },
-            { TOKEN_LITERAL, 5, SYMBOL_POSTS },
+            { TOKEN_LITERAL, 5, LITERAL_USERS },
+            { TOKEN_LITERAL, 5, LITERAL_POSTS },
         },
         .expected_count = 2,
     },
@@ -72,8 +95,8 @@ static lexer_test_t tests[] = {
     {
         .input = "/users/:id",
         .expected = {
-            { TOKEN_LITERAL, 5, SYMBOL_USERS },
-            { TOKEN_PARAM,  2, SYMBOL_ID },
+            { TOKEN_LITERAL, 5, LITERAL_USERS },
+            { TOKEN_PARAM,  2, PARAM_ID },
         },
         .expected_count = 2,
     },
@@ -81,9 +104,9 @@ static lexer_test_t tests[] = {
     {
         .input = "/api/:name/posts",
         .expected = {
-            { TOKEN_LITERAL, 3, SYMBOL_API },
-            { TOKEN_PARAM,   4, SYMBOL_NAME },
-            { TOKEN_LITERAL, 5, SYMBOL_POSTS },
+            { TOKEN_LITERAL, 3, LITERAL_API },
+            { TOKEN_PARAM,   4, PARAM_NAME },
+            { TOKEN_LITERAL, 5, LITERAL_POSTS },
         },
         .expected_count = 3,
     },
@@ -91,7 +114,7 @@ static lexer_test_t tests[] = {
     {
         .input = "/static/*",
         .expected = {
-            { TOKEN_LITERAL, 6, SYMBOL_STATIC },
+            { TOKEN_LITERAL, 6, LITERAL_STATIC },
             { TOKEN_WILDCARD, 1, 0 },
         },
         .expected_count = 2,
@@ -100,10 +123,10 @@ static lexer_test_t tests[] = {
     {
         .input = "/a/b/c/d",
         .expected = {
-            { TOKEN_LITERAL, 1, SYMBOL_A },
-            { TOKEN_LITERAL, 1, SYMBOL_B },
-            { TOKEN_LITERAL, 1, SYMBOL_C },
-            { TOKEN_LITERAL, 1, SYMBOL_D },
+            { TOKEN_LITERAL, 1, LITERAL_A },
+            { TOKEN_LITERAL, 1, LITERAL_B },
+            { TOKEN_LITERAL, 1, LITERAL_C },
+            { TOKEN_LITERAL, 1, LITERAL_D },
         },
         .expected_count = 4,
     },
@@ -111,22 +134,22 @@ static lexer_test_t tests[] = {
     {
         .input = "/v1/users/:id/posts",
         .expected = {
-            { TOKEN_LITERAL, 2, SYMBOL_V1 },
-            { TOKEN_LITERAL, 5, SYMBOL_USERS },
-            { TOKEN_PARAM,  2, SYMBOL_ID },
-            { TOKEN_LITERAL, 5, SYMBOL_POSTS },
+            { TOKEN_LITERAL, 2, LITERAL_V1 },
+            { TOKEN_LITERAL, 5, LITERAL_USERS },
+            { TOKEN_PARAM,  2, PARAM_ID },
+            { TOKEN_LITERAL, 5, LITERAL_POSTS },
         },
         .expected_count = 4,
     },
 
     {
-        .input = "/account/account_id/project/:id/edit",
+        .input = "/account/:account_id/project/:id/edit",
         .expected = {
-            { TOKEN_LITERAL, 7,  SYMBOL_ACCOUNT },
-            { TOKEN_LITERAL, 10, SYMBOL_ACCOUNT_ID },
-            { TOKEN_LITERAL, 7,  SYMBOL_PROJECT },
-            { TOKEN_PARAM,   2,  SYMBOL_ID },
-            { TOKEN_LITERAL, 4,  SYMBOL_EDIT },
+            { TOKEN_LITERAL, 7,  LITERAL_ACCOUNT },
+            { TOKEN_LITERAL, 10, PARAM_ACCOUNT_ID },
+            { TOKEN_LITERAL, 7,  LITERAL_PROJECT },
+            { TOKEN_PARAM,   2,  PARAM_ID },
+            { TOKEN_LITERAL, 4,  LITERAL_EDIT },
         },
         .expected_count = 5,
     },
@@ -134,9 +157,13 @@ static lexer_test_t tests[] = {
 
 static void test_lexer(void)
 {
-    symbol_ctx_t symbols = {0};
-
     size_t test_count = sizeof(tests) / sizeof(tests[0]);
+
+    symbol_ctx_t syms = {0};
+    syms.literals.table = literal_table;
+    syms.literals.count = (uint16_t)(sizeof(literal_table) / sizeof(literal_table[0]));
+    syms.params.table = param_table;
+    syms.params.count = (uint16_t)(sizeof(param_table) / sizeof(param_table[0]));
 
     for (size_t i = 0; i < test_count; i++) {
 
@@ -145,7 +172,7 @@ static void test_lexer(void)
         lexer_init(&lx,
                    tests[i].input,
                    strlen(tests[i].input),
-                   &symbols);
+                   &syms);
 
         for (size_t j = 0; j < tests[i].expected_count; j++) {
 
