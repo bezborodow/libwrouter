d3ab8d6d316509d285dff441c947733a03d1e0ee
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-17 00:08:53 +0930

    Test angle.

--- tests/test_prelexer.c
+++ tests/test_prelexer.c
@@ -136,6 +136,30 @@ static void test_param_brace(void)
     assert(tok.length == 0);
 }
 
+static void test_param_angle(void)
+{
+    pretoken_t tok;
+
+    wrouter_param_syntax_t param_syntax = WROUTER_SYNTAX_ANGLE;
+
+    prelexer_t lx = { 0 };
+    prelexer_init(&lx, param_syntax);
+
+    prelexer_load(&lx, "/accounts/<account_id>");
+
+    tok = prelexer_next(&lx);
+    assert(tok.type == TOKEN_LITERAL);
+    assert(tok.length == 8);
+
+    tok = prelexer_next(&lx);
+    assert(tok.type == TOKEN_PARAM);
+    assert(tok.length == 10);
+
+    tok = prelexer_next(&lx);
+    assert(tok.type == TOKEN_END);
+    assert(tok.length == 0);
+}
+
 int main(void)
 {
     test_root();
@@ -144,5 +168,6 @@ int main(void)
     test_trailing();
     test_double_slash();
     test_param_brace();
+    test_param_angle();
     return 0;
 }
