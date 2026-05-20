e76fc3e6c6f4bf24053b1d461ea228f16a5c29da
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-20 23:13:40 +0930

    Arena align.

--- tests/test_arena.c
+++ tests/test_arena.c
@@ -1,6 +1,14 @@
 #include "arena.h"
 #include <assert.h>
 #include <string.h>
+#include <stdint.h>
+
+typedef struct {
+    double a;
+    double b;
+    double c;
+    char d;
+} test_t;
 
 static void test_arena_basic_alloc(void)
 {
@@ -62,12 +70,62 @@ static void test_arena_free(void)
     assert(a.head == NULL);
 }
 
+static void test_arena_alignment_struct(void)
+{
+    arena_t a = {0};
+
+    test_t *p1 = arena_alloc_aligned(&a, sizeof(test_t), _Alignof(test_t));
+    test_t *p2 = arena_alloc_aligned(&a, sizeof(test_t), _Alignof(test_t));
+
+    assert(p1 != NULL);
+    assert(p2 != NULL);
+
+    assert(((uintptr_t)p1 % _Alignof(test_t)) == 0);
+    assert(((uintptr_t)p2 % _Alignof(test_t)) == 0);
+
+    arena_free(&a);
+}
+
+static void test_arena_mixed_alignment(void)
+{
+    arena_t a = {0};
+
+    char *c = arena_alloc_aligned(&a, 1, _Alignof(char));
+    int *i = arena_alloc_aligned(&a, sizeof(int), _Alignof(int));
+    double *d = arena_alloc_aligned(&a, sizeof(double), _Alignof(double));
+
+    assert(((uintptr_t)c % _Alignof(char)) == 0);
+    assert(((uintptr_t)i % _Alignof(int)) == 0);
+    assert(((uintptr_t)d % _Alignof(double)) == 0);
+
+    arena_free(&a);
+}
+
+static void test_arena_alignment_stress(void)
+{
+    arena_t a = {0};
+
+    for (int i = 0; i < 1000; i++) {
+        size_t align = (i % 64) + 1;
+        if ((align & (align - 1)) != 0)
+            align = 1u << (31 - __builtin_clz(align)); // make power of two
+
+        void *p = arena_alloc_aligned(&a, 17, align);
+        assert(((uintptr_t)p % align) == 0);
+    }
+
+    arena_free(&a);
+}
+
 int main(void)
 {
     test_arena_basic_alloc();
     test_arena_multiple_allocs();
     test_arena_stability_after_growth();
     test_arena_free();
+    test_arena_alignment_struct();
+    test_arena_mixed_alignment();
+    test_arena_alignment_stress();
 
     return 0;
 }
