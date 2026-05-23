b5aeda24a831aac313a163cedf21254ba78164f6
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-23 16:29:23 +0930

    Empty dispatch.

--- src/router.c
+++ src/router.c
@@ -1,6 +1,11 @@
 #include "wrouter.h"
 #include "router.h"
 
+void wrouter_dispatch(const wrouter_t *router, const char *path, void *dispatch_ctx)
+{
+
+}
+
 void wrouter_free(wrouter_t *router)
 {
     free(router);
