2de494c0534fb3a684526161264a8fb1bdc2477e
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-24 23:13:20 +0930

    Check alignments.

--- src/router.h
+++ src/router.h
@@ -35,12 +35,8 @@ struct route {
     void *ctx;
 };
 
-/*
 _Static_assert(sizeof(node_t) % _Alignof(edge_t) == 0, "Node size breaks edge alignment.");
-
-_Static_assert(_Alignof(edge_t) <= _Alignof(node_t),
-               "Edge alignment requirement not satisfied by node alignment.");
-               */
+_Static_assert(sizeof(edge_t) % _Alignof(node_t) == 0, "Edge size breaks node alignment.");
 
 typedef struct symbols {
     const char **base;
