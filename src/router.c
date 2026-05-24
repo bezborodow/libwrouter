6175e7d1c4b696f1b37a8dac28c9ef291c612904
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-24 19:04:56 +0930

    Compile symbols.

--- src/router.c
+++ src/router.c
@@ -8,6 +8,10 @@ void wrouter_dispatch(const wrouter_t *router, const char *path, void *dispatch_
 
 void wrouter_free(wrouter_t *router)
 {
+    free(router->literals.region);
+    free(router->literals.base);
+    free(router->params.region);
+    free(router->params.base);
     free(router->graph);
     free(router);
 }
--- tests/test_symbol.c
+++ tests/test_symbol.c
@@ -22,6 +22,10 @@ static void test_symbol_append(void)
     assert(strcmp(tbl.base[0], "hello") == 0);
     assert(strcmp(tbl.base[1], "world") == 0);
 
+    assert(symbol_resolve("hello", tbl.base, tbl.count) == 1);
+    assert(symbol_resolve("cruel", tbl.base, tbl.count) == 0);
+    assert(symbol_resolve("world", tbl.base, tbl.count) == 2);
+
     symbol_table_free(&tbl);
 }
 
