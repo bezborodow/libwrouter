9282ccfa1b9e0977bbaa75adfd33ba11d6e91fad
Damien Bezborodov <dbezborodov@gmail.com>
2026-05-25 20:13:52 +0930

    Some sort of matching on end.

--- src/router.c
+++ src/router.c
@@ -1,8 +1,10 @@
 #include "wrouter.h"
 #include "router.h"
 #include "lexer.h"
+#include "symbol.h"
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
 void wrouter_dispatch(const wrouter_t *router, const char *path, void *dispatch_ctx)
 {
@@ -14,12 +16,37 @@ void wrouter_ndispatch(const wrouter_t *router, const char *path, size_t length,
     lexer_load(router->lx, path, length);
 
     token_t tok = { 0 };
+    size_t symbol = 0;
+
+    node_t *cur = router->graph, *w_node = NULL;
 
     for (;;) {
         tok = lexer_next(router->lx);
 
-        if (tok.type != TOKEN_LITERAL) {
-            return;
+        switch (tok.type) {
+            case TOKEN_LITERAL:
+                symbol = symbol_resolve(tok.ptr, router->literals.base, router->literals.count);
+                if (cur->flags & NODE_FLAG_HAS_PARAM) {
+                }
+                if (cur->flags & NODE_FLAG_HAS_WILDCARD) {
+                    w_node = cur;
+                }
+                break;
+
+            case TOKEN_END:
+                if (cur->flags & NODE_FLAG_TERMINAL) {
+                    printf("Found terminal.\n");
+                    return;
+                }
+                if (w_node != NULL) {
+                    printf("Found wildcard.\n");
+                }
+                return;
+
+            case TOKEN_ILLEGAL:
+            default:
+                return;
+
         }
     }
 }
