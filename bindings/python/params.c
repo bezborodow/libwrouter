d9f2e6b324639290f6cc847f4432eec56202d930
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-01 02:11:14 +0930

    PyParams work in progress.

--- bindings/python/params.c
+++ bindings/python/params.c
@@ -3,6 +3,41 @@
 #include "params.h"
 #include <Python.h>
 
+static PyObject *PyParams_subscript(PyObject *self, PyObject *key)
+{
+    PyParamsObject *p = (PyParamsObject *)self;
+
+    if (p->snapshot == NULL)
+        return PyErr_Format(PyExc_RuntimeError, "Invalid Params object.");
+
+    if (!PyUnicode_Check(key)) {
+        PyErr_SetString(PyExc_TypeError, "Key must be str.");
+        return NULL;
+    }
+
+    const char *name = PyUnicode_AsUTF8(key);
+    if (!name)
+        return NULL;
+
+    for (uint32_t i = 0; i < p->snapshot->params.count; i++) {
+        wrouter_param_nt_t *param = &p->snapshot->params.base[i];
+
+        if (strcmp(param->name, name) == 0)
+            return PyUnicode_FromString(param->value);
+    }
+
+    PyErr_SetObject(PyExc_KeyError, key);
+
+    return NULL;
+}
+
+static Py_ssize_t PyParams_len(PyObject *self)
+{
+    PyParamsObject *p = (PyParamsObject *)self;
+
+    return p->snapshot->params.count;
+}
+
 PyObject *PyParams_FromDispatcher(const wrouter_dispatcher_t *dispatcher)
 {
     // Deep copy of parameters from the dispatcher.
@@ -29,12 +64,22 @@ failure:
 static void PyParams_dealloc(PyParamsObject *self)
 {
     wrouter_snapshot_free(self->snapshot);
-    PyObject_Del(self);
+
+    Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
+static PyMappingMethods PyParams_mapping = {
+    .mp_length = PyParams_len,
+    .mp_subscript = PyParams_subscript,
+};
+
+// clang-format off
 PyTypeObject PyParamsType = {
-    PyVarObject_HEAD_INIT(NULL, 0).tp_name = "wrouter.Params",
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "wrouter.Params",
     .tp_basicsize = sizeof(PyParamsObject),
     .tp_dealloc = (destructor)PyParams_dealloc,
     .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_as_mapping = &PyParams_mapping,
 };
+// clang-format on
