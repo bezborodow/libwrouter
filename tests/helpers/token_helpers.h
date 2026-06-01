e6e56cbfe09a2a0c9ec65fd86a0b733455d26733
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-01 22:24:12 +0930

    Token test helpers.

---
+++ tests/helpers/token_helpers.h
@@ -0,0 +1,15 @@
+#include "token.h"
+#include <stdio.h>
+#include <assert.h>
+
+#define ASSERT_TOKEN_TYPE(tok, expected_type)                                                \
+    do {                                                                                     \
+        if ((tok).type != (expected_type)) {                                                 \
+            fprintf(stderr, "Expected: %s; got: %s.\n",                                        \
+                token_ident(expected_type),                                        \
+                token_ident(tok.type));                                                          \
+            assert(0);                                                                       \
+        }                                                                                    \
+    } while (0)
+
+const char *token_ident(int type);
