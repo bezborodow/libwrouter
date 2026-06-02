2b943cff7a72bd98a2a8279cb39325974248704b
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-03 05:08:34 +0930

    Const in retain fn.

--- src/router.h
+++ src/router.h
@@ -18,6 +18,6 @@ struct wrouter_router {
     wrouter_reference_fn release;
 };
 
-void router_retain(wrouter_t *router, const wrouter_route_t *route);
+void router_retain(const wrouter_t *router, const wrouter_route_t *route);
 
 const wrouter_route_t *route_match(wrouter_dispatcher_t *d);
