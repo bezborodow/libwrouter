4f9a17978221ea405c7fa930b6a9d821b0f959d9
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-02 18:24:13 +0930

    Random stress test stuff.

--- bindings/python/tests/test_builder.py
+++ bindings/python/tests/test_builder.py
@@ -1,5 +1,7 @@
 import pytest
 import wrouter
+import random
+import string
 
 
 def test_invalid_syntax():
@@ -8,5 +10,16 @@ def test_invalid_syntax():
 
 
 def test_bounds_segment_children():
-    pass
+    builder = wrouter.Builder()
 
+    print("Building...");
+    for i in range(1024):
+        s = ''.join(random.choices(string.ascii_lowercase, k=3))
+        builder.add(f"/{s}-{i}", i)
+
+    print("Compiling...");
+    router = builder.compile()
+    dispatcher = wrouter.Dispatcher(router)
+
+
+    assert dispatcher.resolve("/literal-100") == (10023, {})
