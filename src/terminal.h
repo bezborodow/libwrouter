6b0f2cdd85a6ace00a4f15092b3de6322031cb5e
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-03 04:25:36 +0930

    Move more stuff out of the router.

--- src/terminal.h
+++ src/terminal.h
@@ -6,10 +6,12 @@
  * These are the node terminals, which correspond to a route.  The terminals
  * are referenced by the terminating node's offset.
  */
-typedef struct terminals {
+typedef struct {
     wrouter_route_t *base; // Routes.
     uint16_t *refs;        // List of node offsets, being the key to the route.
     uint16_t count;        // Number of terminals.
 } terminals_t;
 
 wrouter_route_t *terminal_lookup(const terminals_t *terminals, uint16_t ref);
+
+void terminals_free(terminals_t *terminals);
