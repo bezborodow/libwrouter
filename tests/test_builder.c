02653de4dc6f143e1f64ac8bd1c7270089802c37
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-03 13:02:14 +0930

    Test range child count.

--- tests/test_builder.c
+++ tests/test_builder.c
@@ -1,38 +1,121 @@
 #include "wrouter.h"
-#include "router.h"
 #include "builder.h"
+#include "router.h"
 #include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
-static void cb_test(void *dispatch_ctx, const void *route_ctx, const wrouter_params_t *params)
+static void cb_null(void *dispatch_ctx, const void *route_ctx, const wrouter_params_t *params)
 {
-    // TODO Maybe do something here?
     (void)dispatch_ctx;
     (void)route_ctx;
     (void)params;
+
     return;
 }
 
-static void test_builder_add_route(void)
+static void test_free(void)
 {
-    wrouter_options_t options = { .param_syntax = WROUTER_SYNTAX_COLON,
-                                  .fallback_handler = cb_test,
-                                  .fallback_ctx = NULL };
+    wrouter_options_t options = { 0 };
+    wrouter_builder_t *builder = wrouter_builder_create(options);
+
+    assert(builder != NULL);
+
+    wrouter_builder_free(builder);
+
+    // Calling free on NULL is safe.
+    wrouter_builder_free(NULL);
+}
+
+static void test_add_route(void)
+{
+    wrouter_options_t options = { 0 };
     wrouter_builder_t *builder = wrouter_builder_create(options);
 
     assert(builder != NULL);
 
     wrouter_route_t route = { 0 };
-    route.handler = cb_test;
 
-    wrouter_add_route(builder, "/users/{id}", route);
+    assert(wrouter_add_route(builder, "/users/:id", route) == WROUTER_OK);
+
+    wrouter_builder_free(builder);
+}
+
+static void test_add_handler(void)
+{
+    wrouter_options_t options = { 0 };
+    wrouter_builder_t *builder = wrouter_builder_create(options);
+
+    assert(builder != NULL);
+
+    assert(wrouter_add_handler(builder, "/users/:id", cb_null) == WROUTER_OK);
+
+    wrouter_builder_free(builder);
+}
+
+static void test_add_handler_ctx(void)
+{
+    wrouter_options_t options = { 0 };
+    wrouter_builder_t *builder = wrouter_builder_create(options);
+
+    assert(builder != NULL);
+
+    assert(wrouter_add_handler_ctx(builder, "/users/:id", cb_null, NULL) == WROUTER_OK);
+
+    wrouter_builder_free(builder);
+}
+
+static void test_add_context(void)
+{
+    wrouter_options_t options = { 0 };
+    wrouter_builder_t *builder = wrouter_builder_create(options);
+
+    assert(builder != NULL);
+
+    assert(wrouter_add_context(builder, "/users/:id", NULL) == WROUTER_OK);
+
+    wrouter_builder_free(builder);
+}
+
+static void test_range_error_literal_edges(void)
+{
+    char buf[32];
+    uint16_t i;
+    bool range_error = false;
+    wrouter_error_t err;
+    wrouter_options_t options = { 0 };
+
+    wrouter_builder_t *builder = wrouter_builder_create(options);
+
+    assert(builder != NULL);
+
+    for (i = 0; i < UINT16_MAX; i++) {
+        snprintf(buf, sizeof(buf), "/hello_%u", i);
+        err = wrouter_add_context(builder, buf, NULL);
+
+        assert(err == WROUTER_OK || err == WROUTER_ERR_OUT_OF_RANGE);
+        if (err == WROUTER_ERR_OUT_OF_RANGE) {
+            range_error = true;
+            break;
+        }
+    }
+    assert(range_error);
+    assert(i == UINT8_MAX);
 
     wrouter_builder_free(builder);
 }
 
 int main(void)
 {
-    test_builder_add_route();
+    test_free();
+    test_add_route();
+    test_add_handler();
+    test_add_handler_ctx();
+    test_add_context();
+    test_range_error_literal_edges();
 
     return 0;
 }
