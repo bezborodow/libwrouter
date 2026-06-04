8467613af82a6b733905834d1e527ff8a65f5adf
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-04 11:23:32 +0930

    Blank test dispatcher.

---
+++ tests/test_dispatcher.c
@@ -0,0 +1,16 @@
+#include "wrouter.h"
+#include "router.h"
+#include "params.h"
+#include "symbol.h"
+#include "builder.h"
+#include "graph.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    return 0;
+}
