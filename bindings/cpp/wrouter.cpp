290f8fbcedba0f29fafd0e5418a793c68d165a7a
Damien Bezborodov <dbezborodov@gmail.com>
2026-06-04 11:37:45 +0930

    Get C++ example working.

--- bindings/cpp/wrouter.cpp
+++ bindings/cpp/wrouter.cpp
@@ -1,176 +1,147 @@
-#include <wrouter.h>
+#include "wrouter.hpp"
 
-#include <string_view>
-#include <unordered_map>
-#include <stdexcept>
 #include <utility>
 
 namespace wrouter {
 
-class Error : public std::runtime_error {
-public:
-    explicit Error(wrouter_error_t err)
-        : std::runtime_error(wrouter_strerror(err))
-        , code_(err)
-    {}
+Error::Error(wrouter_error_t err)
+    : std::runtime_error(wrouter_strerror(err))
+    , code_(err)
+{}
 
-    wrouter_error_t code() const noexcept { return code_; }
+wrouter_error_t Error::code() const noexcept { return code_; }
 
-private:
-    wrouter_error_t code_;
-};
+Router::Router(wrouter_t *ptr) noexcept
+    : ptr_(ptr)
+{}
 
-class Router {
-public:
-    explicit Router(wrouter_t *ptr = nullptr) noexcept
-        : ptr_(ptr)
-    {}
+Router::~Router()
+{
+    if (ptr_)
+        wrouter_destroy(&ptr_);
+}
 
-    ~Router() {
+Router::Router(Router&& rhs) noexcept
+    : ptr_(std::exchange(rhs.ptr_, nullptr))
+{}
+
+Router& Router::operator=(Router&& rhs) noexcept
+{
+    if (this != &rhs) {
         if (ptr_)
             wrouter_destroy(&ptr_);
+
+        ptr_ = std::exchange(rhs.ptr_, nullptr);
     }
 
-    Router(Router&& rhs) noexcept
-        : ptr_(std::exchange(rhs.ptr_, nullptr))
-    {}
+    return *this;
+}
 
-    Router& operator=(Router&& rhs) noexcept {
-        if (this != &rhs) {
-            if (ptr_)
-                wrouter_destroy(&ptr_);
-            ptr_ = std::exchange(rhs.ptr_, nullptr);
-        }
-        return *this;
-    }
+wrouter_t *Router::native() const noexcept { return ptr_; }
 
-    Router(const Router&) = delete;
-    Router& operator=(const Router&) = delete;
+Builder::Builder(const wrouter_options_t& opts)
+{
+    ptr_ = wrouter_builder_create(opts);
 
-    wrouter_t *native() const noexcept { return ptr_; }
+    if (!ptr_)
+        throw std::bad_alloc{};
+}
 
-private:
-    wrouter_t *ptr_;
-};
+Builder::~Builder()
+{
+    if (ptr_)
+        wrouter_builder_destroy(&ptr_);
+}
 
-class Builder {
-public:
-    explicit Builder(const wrouter_options_t& opts = {})
-    {
-        ptr_ = wrouter_builder_create(opts);
+Builder::Builder(Builder&& rhs) noexcept
+    : ptr_(std::exchange(rhs.ptr_, nullptr))
+{}
 
-        if (!ptr_)
-            throw std::bad_alloc{};
-    }
-
-    ~Builder() {
+Builder& Builder::operator=(Builder&& rhs) noexcept
+{
+    if (this != &rhs) {
         if (ptr_)
             wrouter_builder_destroy(&ptr_);
+
+        ptr_ = std::exchange(rhs.ptr_, nullptr);
     }
 
-    Builder(Builder&& rhs) noexcept
-        : ptr_(std::exchange(rhs.ptr_, nullptr))
-    {}
+    return *this;
+}
 
-    Builder& operator=(Builder&& rhs) noexcept {
-        if (this != &rhs) {
-            if (ptr_)
-                wrouter_builder_destroy(&ptr_);
+Builder& Builder::add(std::string_view pattern,
+                      wrouter_handler_fn fn,
+                      const void *ctx)
+{
+    wrouter_error_t err =
+        wrouter_add_handler_ctx(ptr_, pattern.data(), fn, ctx);
 
-            ptr_ = std::exchange(rhs.ptr_, nullptr);
-        }
+    if (err)
+        throw Error(err);
 
-        return *this;
+    return *this;
+}
+
+Builder& Builder::add_context(std::string_view pattern,
+                              const void *ctx)
+{
+    auto err = wrouter_add_context(ptr_, pattern.data(), ctx);
+
+    if (err)
+        throw Error(err);
+
+    return *this;
+}
+
+Router Builder::consume()
+{
+    wrouter_error_t err = WROUTER_OK;
+
+    wrouter_t *router =
+        wrouter_consume(&ptr_, &err);
+
+    if (err)
+        throw Error(err);
+
+    return Router(router);
+}
+
+Dispatcher::Dispatcher(const Router& router)
+{
+    ptr_ = wrouter_dispatcher_create(router.native());
+
+    if (!ptr_)
+        throw std::bad_alloc{};
+}
+
+Dispatcher::~Dispatcher()
+{
+    if (ptr_)
+        wrouter_dispatcher_destroy(&ptr_);
+}
+
+void Dispatcher::dispatch(std::string_view path,
+                          void *dispatch_ctx)
+{
+    wrouter_dispatch(ptr_, path.data(), dispatch_ctx);
+}
+
+std::unordered_map<std::string, std::string> Dispatcher::params() const
+{
+    std::unordered_map<std::string, std::string> out;
+
+    auto *p = wrouter_params(ptr_);
+
+    for (uint32_t i = 0; i < p->count; ++i) {
+        auto &v = p->base[i];
+
+        out.emplace(
+            v.name,
+            std::string(v.value, v.length)
+        );
     }
 
-    Builder(const Builder&) = delete;
-    Builder& operator=(const Builder&) = delete;
-
-    Builder& add(std::string_view pattern,
-                 wrouter_handler_fn fn,
-                 const void *ctx = nullptr)
-    {
-        wrouter_error_t err =
-            wrouter_add_handler_ctx(ptr_, pattern.data(), fn, ctx);
-
-        if (err)
-            throw Error(err);
-
-        return *this;
-    }
-
-    Builder& add_context(std::string_view pattern,
-                         const void *ctx)
-    {
-        auto err = wrouter_add_context(ptr_, pattern.data(), ctx);
-
-        if (err)
-            throw Error(err);
-
-        return *this;
-    }
-
-    Router consume()
-    {
-        wrouter_error_t err = WROUTER_OK;
-
-        wrouter_t *router =
-            wrouter_consume(&ptr_, &err);
-
-        if (err)
-            throw Error(err);
-
-        return Router(router);
-    }
-
-private:
-    wrouter_builder_t *ptr_ {};
-};
-
-class Dispatcher {
-public:
-    explicit Dispatcher(const Router& router)
-    {
-        ptr_ = wrouter_dispatcher_create(router.native());
-
-        if (!ptr_)
-            throw std::bad_alloc{};
-    }
-
-    ~Dispatcher() {
-        if (ptr_)
-            wrouter_dispatcher_destroy(&ptr_);
-    }
-
-    Dispatcher(const Dispatcher&) = delete;
-    Dispatcher& operator=(const Dispatcher&) = delete;
-
-    void dispatch(std::string_view path,
-                  void *dispatch_ctx = nullptr)
-    {
-        wrouter_dispatch(ptr_, path.data(), dispatch_ctx);
-    }
-
-    std::unordered_map<std::string,std::string> params() const
-    {
-        std::unordered_map<std::string,std::string> out;
-
-        auto *p = wrouter_params(ptr_);
-
-        for (uint32_t i = 0; i < p->count; ++i) {
-            auto &v = p->base[i];
-
-            out.emplace(
-                v.name,
-                std::string(v.value, v.length)
-            );
-        }
-
-        return out;
-    }
-
-private:
-    wrouter_dispatcher_t *ptr_ {};
-};
+    return out;
+}
 
 }
